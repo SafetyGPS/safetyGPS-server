6a2ebc77ab0bf6dd97399b101e37eeb7cb484a23
Jooyunbin <dkdltm221@naver.com>
2025-11-05 15:33:09 +0900

    chore: initialize SafetyGPS server project

---
+++ src/main/java/com/safetygps/safetygps_server/SafetygpsServerApplication.java
@@ -0,0 +1,13 @@
+package com.safetygps.safetygps_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetygpsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetygpsServerApplication.class, args);
+	}
+
+}
