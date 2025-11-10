1e8cd82dda2cb69b53b25a9292e6e81eafc62ab7
Jooyunbin <dkdltm221@naver.com>
2025-11-10 20:41:53 +0900

    feat/#8: 코드래빗 리뷰 반영

--- src/main/java/com/safetygps/safetygps_server/controller/facility/FacilityController.java
+++ src/main/java/com/safetygps/safetygps_server/controller/facility/FacilityController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -18,7 +19,7 @@ public interface FacilityController {
             summary = "시설 데이터 동기화",
             description = "경기도 공공데이터 포털로부터 시설 정보를 조회하고 DB에 저장합니다."
     )
-    @GetMapping("/api/facilities/sync")
+    @PostMapping("/api/facilities/sync")
     ResponseEntity<String> syncFromOpenApi(
             @Parameter(description = "시군명 (예: 수원시, 고양시)", example = "수원시")
             @RequestParam String sigunNm
